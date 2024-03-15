143c9e3f260c854d050aceb2d4c025351154fbff
Alejandro Olivera <aienma325@gmail.com>
2024-03-15 17:40:18 -0500

    first produuct

--- src/sections/Product/Product.tsx
+++ src/sections/Product/Product.tsx
@@ -1,15 +1,21 @@
 import { useState } from 'react';
+import { Modal } from '@mantine/core';
+
 import { WText } from '@/components/WText/WText';
 import classes from './Product.module.css';
 import { Frame } from '@/components/WFrame/WFrame';
+import { WButton } from '@/components/WButton/WButton';
 
-export const Product = () => {
-  const [isTrue, setIsTrue] = useState(false);
+interface IProduct {}
+
+export const Product: React.FC<IProduct> = () => {
+  const [isShowModal, setIsShowModal] = useState(false);
+
   return (
     <section className={classes.product}>
       <div className={classes.headerProduct}>
         <div>
-          <Frame styles={classes.frame}>
+          <Frame styles={classes.frameSelect}>
             <img src="/test.png" alt="headphone" />
           </Frame>
 
@@ -26,10 +32,27 @@ export const Product = () => {
           </Frame>
         </div>
 
-        <div className={classes.imageProduct}>
+        <div className={classes.imageProductContainer}>
           <img src="/test.png" alt="headphone" />
         </div>
       </div>
+
+      <div>
+        <WText text="Lorem ipsu Lorem ipsu Lorem ipsu Lorem ipsu Lorem ipsu" />
+        <WText text="Iphone" />
+        <WText text="Iphone" />
+      </div>
+
+      <div className={classes.buttonContainer}>
+        <WText text="$50.09" />
+        <WButton
+          styles={classes.buttonProduct}
+          title="Paid the product"
+          onClick={() => setIsShowModal(true)}
+        />
+      </div>
+
+      <Modal fullScreen opened={isShowModal} onClose={() => setIsShowModal(false)} />
     </section>
   );
 };
